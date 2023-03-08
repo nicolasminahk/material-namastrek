ade1eedd3d8945693ce0abf397c983da31543d55
nicolasminahk <nico.minahk@gmail.com>
2023-03-07 21:43:38 -0300

    Advanced view Cards

--- src/components/ExitView.jsx
+++ src/components/ExitView.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box, Typography, Card, CardContent, CardMedia, Button } from '@mui/material'
 import { Rating } from '@mui/material'
 import { useSpring, animated } from 'react-spring'
+import backgroundImage from '../assets/paisaje3.png'
 
 function ExitView({ name, description, image, price, rating }) {
     const [hovered, setHovered] = React.useState(false)
@@ -18,26 +19,57 @@ function ExitView({ name, description, image, price, rating }) {
                 flexDirection: 'column',
                 alignItems: 'center',
                 mb: 4,
-                backgroundImage: `url(${image})`,
+                // backgroundImage: `url(${image})`,
+                // backgroundImage: backgroundImage,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
-                backgroundColor: '#ECEFF1',
                 borderRadius: '10px',
                 overflow: 'hidden',
+                padding: '20px',
+                margin: '10px',
+                border: '1px solid #ffff',
+                '@media (max-width: 768px)': {
+                    padding: '10px',
+                    margin: '5px',
+                },
             }}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
         >
             <animated.div style={hoverAnimation}>
-                <Card variant="outlined" sx={{ maxWidth: 600, backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
-                    <CardContent sx={{ display: 'flex', flexDirection: 'column', color: 'white' }}>
-                        <Typography variant="h4" sx={{ mb: 2 }}>
+                <Card
+                    variant="outlined"
+                    sx={{
+                        maxWidth: 400,
+                        backgroundColor: 'transparent',
+                        boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.2)',
+                        borderRadius: 12,
+                    }}
+                >
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            color: 'white',
+                            flexGrow: 1,
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <CardMedia
+                            component="img"
+                            height="100%"
+                            image={backgroundImage}
+                            alt="output"
+                            style={{ objectFit: 'cover', borderRadius: 30 }}
+                        />
+
+                        <Typography variant="h4" sx={{ mb: 2, color: 'green', paddingTop: '5px' }}>
                             {name}
                         </Typography>
-                        <Typography variant="body1" sx={{ mb: 2 }}>
+                        <Typography variant="body1" sx={{ mb: 2, color: 'green' }}>
                             {description}
                         </Typography>
-                        <Typography variant="body1" sx={{ mb: 2 }}>
+                        <Typography variant="body1" sx={{ mb: 2, color: 'green' }}>
                             Price: {price}
                         </Typography>
                         {/* <Typography variant="body1" sx={{ mb: 2 }}>
