94cdbc58c219dc8badbb75c08df1059f108585d8
nicolasminahk <nico.minahk@gmail.com>
2023-03-07 21:42:23 -0300

    Structure advanced

--- src/components/AdminProfile.jsx
+++ src/components/AdminProfile.jsx
@@ -1,104 +1,124 @@
 import React, { useState } from 'react'
-import { useMutation } from '@apollo/client'
 import { Button, TextField, Typography } from '@mui/material'
-import { ADD_BENEFIT, DELETE_BENEFIT, UPDATE_BENEFIT } from './graphql/mutations'
 
 const AdministratorProfile = () => {
+    const [outputs, setOutputs] = useState([])
     const [benefits, setBenefits] = useState([])
+    const [tips, setTips] = useState([])
 
-    const [newBenefitName, setNewBenefitName] = useState('')
-    const [newBenefitDescription, setNewBenefitDescription] = useState('')
-    const [newBenefitDate, setNewBenefitDate] = useState('')
+    const [newOutput, setNewOutput] = useState('')
+    const [newBenefit, setNewBenefit] = useState('')
+    const [newTip, setNewTip] = useState('')
 
-    const [addBenefitMutation] = useMutation(ADD_BENEFIT)
-    const [deleteBenefitMutation] = useMutation(DELETE_BENEFIT)
-    const [updateBenefitMutation] = useMutation(UPDATE_BENEFIT)
-
-    const addBenefit = async () => {
-        const { data } = await addBenefitMutation({
-            variables: {
-                name: newBenefitName,
-                description: newBenefitDescription,
-                date: newBenefitDate,
-            },
-        })
-        setBenefits([...benefits, data.addBenefits])
-        setNewBenefitName('')
-        setNewBenefitDescription('')
-        setNewBenefitDate('')
+    const addOutput = () => {
+        setOutputs([...outputs, newOutput])
+        setNewOutput('')
     }
 
-    const deleteBenefit = async (id) => {
-        await deleteBenefitMutation({
-            variables: { id },
-        })
-        setBenefits(benefits.filter((benefit) => benefit.id !== id))
+    const addBenefit = () => {
+        setBenefits([...benefits, newBenefit])
+        setNewBenefit('')
     }
 
-    const updateBenefit = async (id, name, description, date) => {
-        await updateBenefitMutation({
-            variables: { id, name, description, date },
-        })
-        setBenefits(benefits.map((benefit) => (benefit.id === id ? { id, name, description, date } : benefit)))
+    const addTip = () => {
+        setTips([...tips, newTip])
+        setNewTip('')
     }
 
-    return ''
+    const deleteOutput = (index) => {
+        setOutputs(outputs.filter((_, i) => i !== index))
+    }
+
+    const deleteBenefit = (index) => {
+        setBenefits(benefits.filter((_, i) => i !== index))
+    }
+
+    const deleteTip = (index) => {
+        setTips(tips.filter((_, i) => i !== index))
+    }
+
+    const updateOutput = (index, newValue) => {
+        setOutputs(outputs.map((value, i) => (i === index ? newValue : value)))
+    }
+
+    const updateBenefit = (index, newValue) => {
+        setBenefits(benefits.map((value, i) => (i === index ? newValue : value)))
+    }
+
+    const updateTip = (index, newValue) => {
+        setTips(tips.map((value, i) => (i === index ? newValue : value)))
+    }
+
+    return (
+        <div sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Typography variant="h4">Administrator Profile</Typography>
+
+            <Typography variant="h5">Outputs</Typography>
+            <div sx={{ display: 'flex', flexDirection: 'column' }}>
+                {outputs.map((output, index) => (
+                    <div key={index} sx={{ display: 'flex', alignItems: 'center' }}>
+                        <TextField
+                            value={output}
+                            onChange={(event) => updateOutput(index, event.target.value)}
+                            sx={{ mr: 2 }}
+                        />
+                        <Button variant="contained" color="error" onClick={() => deleteOutput(index)}>
+                            Delete
+                        </Button>
+                    </div>
+                ))}
+                <div sx={{ display: 'flex', alignItems: 'center' }}>
+                    <TextField
+                        value={newOutput}
+                        onChange={(event) => setNewOutput(event.target.value)}
+                        sx={{ mr: 2 }}
+                    />
+                    <Button variant="contained" color="success" onClick={addOutput}>
+                        Add
+                    </Button>
+                </div>
+            </div>
+
+            <div sx={{ display: 'flex', flexDirection: 'column' }}>
+                {benefits.map((benefit, index) => (
+                    <div key={index} sx={{ display: 'flex', alignItems: 'center' }}>
+                        <TextField
+                            value={benefit}
+                            onChange={(event) => updateBenefit(index, event.target.value)}
+                            sx={{ mr: 2 }}
+                        />
+                        <Button variant="contained" color="error" onClick={() => deleteBenefit(index)}>
+                            Delete
+                        </Button>
+                    </div>
+                ))}
+                <div sx={{ display: 'flex', alignItems: 'center' }}>
+                    <TextField
+                        value={newOutput}
+                        onChange={(event) => setNewBenefit(event.target.value)}
+                        sx={{ mr: 2 }}
+                    />
+                    <Button variant="contained" color="success" onClick={addBenefit}>
+                        Add
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
 }
-// <div sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-//   <Typography variant="h4">Administrator Profile</Typography>
+{
+    /* <Typography variant="h5">Benefits</Typography>
+      <div sx={{ display: 'flex', flexDirection: 'column' }}>
+        {benefits.map((benefit, index) => (
+          <div key={index} sx={{ display: 'flex', alignItems: 'center' }}>
+            <TextField
+              value={benefit}
+              onChange={(event) => updateBenefit(index, event.target.value)}
+              sx={{ mr: 2 }}
+            />
+            <Button variant="contained" color="error" onClick={() => deleteBenefit(index)}>
+              Delete
+            </> */
+}
 
-//   <Typography variant="h5">Benefits</Typography>
-//   <div sx={{ display: 'flex', flexDirection: 'column' }}>
-//     {benefits.map((benefit) => (
-//       <div key={benefit.id} sx={{ display: 'flex', alignItems: 'center' }}>
-//         <TextField
-//           value={benefit.name}
-//           onChange={(event) =>
-//             updateBenefit(
-//               benefit.id,
-//               event.target.value,
-//               benefit.description,
-//               benefit.date
-//             )
-//           }
-//           sx={{ mr: 2 }}
-//         />
-//         <TextField
-//           value={benefit.description}
-//           onChange={(event) =>
-//             updateBenefit(
-//               benefit.id,
-//               benefit.name,
-//               event.target.value,
-//               benefit.date
-//             )
-//           }
-//           sx={{ mr: 2 }}
-//         />
-//         <TextField
-//           value={benefit.date}
-//           onChange={(event) =>
-//             updateBenefit(
-//               benefit.id,
-//               benefit.name,
-//               benefit.description,
-//               event.target.value
-//             )
-//           }
-//           sx={{ mr: 2 }}
-//         />
-//         <Button variant="contained" color="error" onClick={() => deleteBenefit(benefit.id)}>
-//           Delete
-//         </Button>
-//       </div>
-//     ))}
-//     <div sx={{ display: 'flex', alignItems: 'center' }}>
-//       <TextField
-//         value={newBenefitName}
-//         onChange={(event) => setNewBenefitName(event.target.value)}
-//         sx={{ mr: 2 }}
-//       />
-//       <TextField
-//         value
-
-//         ></TextField>
+export default AdministratorProfile
