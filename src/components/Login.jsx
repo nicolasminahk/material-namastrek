2fec7fa46e023c6105c844dae1bdf3f0914511a6
nicolasminahk <nico.minahk@gmail.com>
2023-03-21 17:12:33 -0300

    Send data create new user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Button } from '@mui/material'
-// import { gql, useQuery, useMutation } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
 
-// const CREATE_USER = gql`
-//     mutation Mutation($createUserId: ID!, $email: String!) {
-//         createUser(id: $createUserId, email: $email) {
-//             id
-//             email
-//         }
-//     }
-// `
+const CREATE_USER = gql`
+    mutation Mutation($createUserId: ID!, $email: String!) {
+        createUser(id: $createUserId, email: $email) {
+            id
+            email
+        }
+    }
+`
 //PARA EXTRAER LAS LETRAS Y DEJAR LAS LETRAS PARA UTILIZAR COMO ID
 function extractNumbers(inputString) {
     return inputString.replace(/\D/g, '')
 }
 export const LoginButton = () => {
     const { loginWithRedirect, user, isAuthenticated } = useAuth0()
+    const [userId, setUserID] = useState('')
+    const [email, setEmail] = useState('')
     console.log(isAuthenticated)
     if (isAuthenticated) {
         console.log(user)
+        const userIdDepure = extractNumbers(user.sub)
+        setUserID(userIdDepure)
+        setEmail(user.email)
     }
-    // const [createUser] = useMutation(CREATE_USER, {
-    //     variables: {
-    //         id: extractNumbers(user.sub)
-    //     },
-    // })
+
+    const [createUser] = useMutation(CREATE_USER, {
+        variables: {
+            id: userId,
+            email: email,
+        },
+    })
+
+    const handleDelete = () => {
+        createUser()
+        refetch()
+    }
     return <Button onClick={() => loginWithRedirect()}>Login</Button>
 }
