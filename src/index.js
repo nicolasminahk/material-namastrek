de7c9fde584ff319e83d9c42a7be6f078a860401
nicolasminahk <nico.minahk@gmail.com>
2023-03-27 17:06:34 -0300

    Feat  Token

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
@@ -6,15 +6,36 @@ import theme from './config/theme.js'
 import { ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
-import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider, createHttpLink, ApolloLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { Cookies } from 'react-cookie'
+
+const cookies = new Cookies()
+// const client = new ApolloClient({
+//     cache: new InMemoryCache(),
+//     link: new HttpLink({
+//         uri: 'http://localhost:4000',
+//     }),
+// })
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000',
+})
+
+const authLink = setContext((_, { headers }) => {
+    const token = cookies.get('token')
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    }
+})
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000',
-    }),
+    link: authLink.concat(httpLink),
 })
-
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
 const redirectUri = isMobile ? 'http://192.168.100.5:3000' : window.location.origin
