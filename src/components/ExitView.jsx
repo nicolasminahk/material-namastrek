d99ca9505242fa3e25143daf71adc3468377ff5b
nicolasminahk <nico.minahk@gmail.com>
2023-04-12 20:56:47 -0300

    Add person Exit

--- src/components/ExitView.jsx
+++ src/components/ExitView.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Box, Typography, Card, CardContent, CardMedia, Button } from '@mui/material'
 import { Rating } from '@mui/material'
 import { useSpring, animated } from 'react-spring'
 import backgroundImage from '../assets/paisaje3.png'
+import { gql, useQuery, useMutation } from '@apollo/client'
+import { useAuth0 } from '@auth0/auth0-react'
 
-function ExitView({ name, description, image, price, date }) {
+const ADD_PERSON_EXIT = gql`
+    mutation AddPersonExit($salida: String!, $auth0UserId: String!) {
+        addPersonExit(salida: $salida, auth0UserId: $auth0UserId) {
+            name
+            date
+            id
+            price
+        }
+    }
+`
+function extractNumbers(inputString) {
+    return inputString?.replace(/\D/g, '')
+}
+
+function ExitView({ name, description, image, price, date, id }) {
     const [hovered, setHovered] = React.useState(false)
+    const [idSalida, setIdSalida] = useState('')
+    const { user, isAuthenticated, error: errorAuth0, isLoading: loadingAuth0 } = useAuth0()
+    const userDepure = extractNumbers(user?.sub)
+    console.log(userDepure)
+    console.log(idSalida)
+
+    const [addPersonExit] = useMutation(ADD_PERSON_EXIT, {
+        variables: {
+            auth0UserId: userDepure,
+            salida: idSalida,
+        },
+    })
+
+    // useEffect(() => {
+    //     addPersonExit()
+    // }, [idSalida])
 
     const hoverAnimation = useSpring({
         transform: hovered ? 'scale(1.02)' : 'scale(1)',
         boxShadow: hovered ? '0px 10px 20px rgba(0, 0, 0, 0.2)' : 'none',
     })
-
     return (
         <Box
             sx={{
@@ -23,7 +54,7 @@ function ExitView({ name, description, image, price, date }) {
                 backgroundPosition: 'center',
                 borderRadius: '10px',
                 overflow: 'hidden',
-                padding: '20px',
+                padding: '10px',
                 margin: '10px',
                 border: '1px solid #ffff',
                 '@media (max-width: 768px)': {
@@ -38,7 +69,7 @@ function ExitView({ name, description, image, price, date }) {
                 <Card
                     variant="outlined"
                     sx={{
-                        maxWidth: 400,
+                        maxWidth: 380,
                         backgroundColor: 'transparent',
                         boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.2)',
                         borderRadius: 12,
@@ -70,6 +101,9 @@ function ExitView({ name, description, image, price, date }) {
                         <Typography variant="body1" sx={{ mb: 2, color: 'green' }}>
                             {date}
                         </Typography>
+                        {/* <Typography variant="body1" sx={{ mb: 2, color: 'green' }}>
+                            {id}
+                        </Typography> */}
                         <Typography variant="body1" sx={{ mb: 2, color: 'green' }}>
                             Price: {price}
                         </Typography>
@@ -83,6 +117,10 @@ function ExitView({ name, description, image, price, date }) {
                                 mt: 2,
                                 alignSelf: 'flex-end',
                                 '@media (max-width:600px)': { alignSelf: 'center', mt: 4 },
+                            }}
+                            onClick={() => {
+                                setIdSalida(id)
+                                addPersonExit()
                             }}
                         >
                             Reservar Ahora
