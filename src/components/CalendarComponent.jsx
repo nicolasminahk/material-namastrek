6b91b152a44ddbb4a1f0f4fbb329aaeaf7ad5522
nicolasminahk <nico.minahk@gmail.com>
2023-04-30 21:05:50 -0300

    Add only user login

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { Calendar } from 'react-calendar'
-import { Box, Divider, Typography } from '@mui/material'
+import { Box, Button, Divider, Typography } from '@mui/material'
 import styled from 'styled-components'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
+import { useAuth0 } from '@auth0/auth0-react'
+import { gql, useMutation } from '@apollo/client'
 
 const StyledDot = styled(Box)`
     height: 10px;
@@ -17,11 +19,34 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.tz.setDefault('America/Argentina/Buenos_Aires')
 
+const ADD_PERSON_EXIT = gql`
+    mutation AddPersonExit($salida: String!, $auth0UserId: String!) {
+        addPersonExit(salida: $salida, auth0UserId: $auth0UserId) {
+            name
+            date
+            id
+            price
+        }
+    }
+`
+function extractNumbers(inputString) {
+    return inputString?.replace(/\D/g, '')
+}
 const CalendarComponent = ({ activities }) => {
     const [selectedDate, setSelectedDate] = useState(null)
     const [date, setDate] = useState(new Date())
-    console.log(activities)
-    console.log(selectedDate)
+    const { user, isAuthenticated, error: errorAuth0, isLoading: loadingAuth0 } = useAuth0()
+    const userDepure = extractNumbers(user?.sub)
+    const [idSalida, setIdSalida] = useState('')
+    // console.log(activities)
+    // console.log(selectedDate)
+
+    const [addPersonExit] = useMutation(ADD_PERSON_EXIT, {
+        variables: {
+            auth0UserId: userDepure,
+            salida: idSalida,
+        },
+    })
 
     const renderDay = (date, _view) => {
         const activitiesOnDay = activities?.filter((activity) => {
@@ -46,20 +71,6 @@ const CalendarComponent = ({ activities }) => {
         weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
     }
 
-    // const filteredActivities = selectedDate
-    //     ? activities?.filter((activity) => {
-    //           return dayjs(activity.date).isSame(dayjs(selectedDate), 'day')
-    //       })
-    //     : []
-    // const filteredActivities = selectedDate
-    //     ? activities?.filter((activity) => {
-    //           const selectedDateISO = selectedDate.toISOString()
-    //           const activityDateISO = dayjs(activity.date).format('YYYY-MM-DD[T]HH:mm:ss.SSS[Z]')
-    //           console.log(selectedDateISO)
-    //           console.log(activityDateISO)
-    //           return selectedDateISO === activityDateISO
-    //       })
-    //     : []
     const filteredActivities = selectedDate
         ? activities?.filter((activity) => {
               const activityDate = dayjs(activity.date).tz()
@@ -74,8 +85,6 @@ const CalendarComponent = ({ activities }) => {
 
     return (
         <Box sx={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'colum' }}>
-            {/* <div className="dayOfWeek">{getDayOfWeek()}</div>
-            <div className="date">{date.toLocaleDateString()}</div> */}
             <CalendarContainer>
                 <Calendar
                     value={selectedDate}
@@ -86,10 +95,7 @@ const CalendarComponent = ({ activities }) => {
                             return dayjs(activity.date).isSame(date, 'day')
                         })
 
-                        return activitiesOnDay?.length > 0 ? (
-                            // <StyledDot position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" />
-                            <StyledDot position="absolute" />
-                        ) : null
+                        return activitiesOnDay?.length > 0 ? <StyledDot position="absolute" /> : null
                     }}
                     tileClassName={({ date }) => {
                         const activitiesOnDay = activities?.filter((activity) => {
@@ -135,6 +141,24 @@ const CalendarComponent = ({ activities }) => {
                             <Typography>{activity.description}</Typography>
                             <Typography>{activity.date}</Typography>
                             <Typography>{activity.price}</Typography>
+                            <Typography>{activity.duration}</Typography>
+                            {user && (
+                                <Button
+                                    variant="contained"
+                                    color="success"
+                                    sx={{
+                                        mt: 2,
+                                        alignSelf: 'flex-end',
+                                        '@media (max-width:600px)': { alignSelf: 'center', mt: 4 },
+                                    }}
+                                    onClick={() => {
+                                        setIdSalida(activity.id)
+                                        addPersonExit()
+                                    }}
+                                >
+                                    Reservar Ahora
+                                </Button>
+                            )}
                             <Divider style={{ color: 'green' }} />
                         </Box>
                     ))}
@@ -146,126 +170,6 @@ const CalendarComponent = ({ activities }) => {
 
 export default CalendarComponent
 
-// const CalendarContainer = styled.div`
-//     /* ~~~ container styles ~~~ */
-//     max-width: 600px;
-//     margin: auto;
-//     margin-top: 20px;
-//     background-color: #d4f7d4;
-//     padding: 10px;
-//     border-radius: 3px;
-
-//     /* ~~~ navigation styles ~~~ */
-//     .react-calendar__navigation {
-//         display: flex;
-
-//         .react-calendar__navigation__label {
-//             font-weight: bold;
-//             flex-grow: 1;
-//             text-align: center;
-//         }
-
-//         .react-calendar__navigation__arrow {
-//             flex-grow: 0.333;
-//         }
-//     }
-
-//     /* ~~~ label styles ~~~ */
-//     .react-calendar__month-view__weekdays {
-//         display: grid;
-//         grid-template-columns: repeat(7, 1fr);
-//         text-align: center;
-//         margin-bottom: 10px;
-//     }
-
-//     /* ~~~ button styles ~~~ */
-//     button {
-//         margin: 3px;
-//         background-color: #6f876f;
-//         border: 0;
-//         border-radius: 3px;
-//         color: white;
-//         padding: 5px 0;
-
-//         &:hover {
-//             background-color: #556b55;
-//         }
-
-//         &:active {
-//             background-color: #a5c1a5;
-//         }
-//     }
-
-//     /* ~~~ day grid styles ~~~ */
-//     .react-calendar__month-view__days {
-//         display: grid !important;
-//         grid-template-columns: repeat(7, 1fr);
-
-//         .react-calendar__tile {
-//             max-width: initial !important;
-//             display: flex;
-//             flex-direction: column;
-//             align-items: center;
-//             justify-content: center;
-//             height: 70px;
-//             padding: 5px;
-//             border-radius: 50%;
-//             cursor: pointer;
-
-//             &.react-calendar__tile--active {
-//                 background-color: #6f876f;
-//                 color: white;
-
-//                 &:hover {
-//                     background-color: #556b55;
-//                 }
-//             }
-
-//             &.react-calendar__tile--now {
-//                 background-color: #f0f0f0;
-//             }
-
-//             &.react-calendar__tile--weekend {
-//                 color: #dfdfdf;
-//             }
-
-//             &.react-calendar__tile--neighboringMonth {
-//                 opacity: 0.5;
-//             }
-//         }
-//     }
-
-//     /* ~~~ neighboring month styles ~~~ */
-//     .react-calendar__tile--neighboringMonth {
-//         opacity: 0.5;
-//     }
-
-//     /* ~~~ weekend styles ~~~ */
-//     .react-calendar__tile--weekend {
-//         color: #dfdfdf;
-//     }
-
-//     /* ~~~ active day styles ~~~ */
-//     .react-calendar__tile--range {
-//         box-shadow: 0 0 6px 2px black;
-//     }
-
-//     /* ~~~ other view styles ~~~ */
-//     .react-calendar__year-view__months,
-//     .react-calendar__decade-view__years,
-//     .react-calendar__century-view__decades {
-//         display: grid !important;
-//         grid-template-columns: 20% 20% 20% 20% 20%;
-
-//         &.react-calendar__year-view__months {
-//             grid-template-columns: 33.3% 33.3% 33.3%;
-//         }
-
-//         .react-calendar__tile {
-//             max-width: initial !important;
-//         }
-//     }
-// `
 const CalendarContainer = styled.div`
     max-width: 600px;
     margin: auto;
@@ -407,94 +311,3 @@ const CalendarContainer = styled.div`
         }
     }
 `
-
-// const CalendarContainer = styled.div`
-//     /* ~~~ container styles ~~~ */
-//     max-width: 600px;
-//     margin: auto;
-//     margin-top: 20px;
-//     background-color: #d4f7d4;
-//     padding: 10px;
-//     border-radius: 3px;
-//     /* ~~~ navigation styles ~~~ */
-//     .react-calendar__navigation {
-//         display: flex;
-
-//         .react-calendar__navigation__label {
-//             font-weight: bold;
-//         }
-
-//         .react-calendar__navigation__arrow {
-//             flex-grow: 0.333;
-//         }
-//     }
-//     /* ~~~ label styles ~~~ */
-//     .react-calendar__month-view__weekdays {
-//         text-align: center;
-//     }
-//     /* ~~~ button styles ~~~ */
-//     button {
-//         margin: 3px;
-//         background-color: #6f876f;
-//         border: 0;
-//         border-radius: 3px;
-//         color: white;
-//         padding: 5px 0;
-
-//         &:hover {
-//             background-color: #556b55;
-//         }
-
-//         &:active {
-//             background-color: #a5c1a5;
-//         }
-//     }
-//     /* ~~~ day grid styles ~~~ */
-//     .react-calendar__month-view__days {
-//         display: grid !important;
-//         grid-template-columns: 14.2% 14.2% 14.2% 14.2% 14.2% 14.2% 14.2%;
-
-//         .react-calendar__tile {
-//             max-width: initial !important;
-//         }
-//     }
-//     /* ~~~ neighboring month & weekend styles ~~~ */
-//     .react-calendar__month-view__days__day--neighboringMonth {
-//         opacity: 0.7;
-//     }
-//     .react-calendar__month-view__days__day--weekend {
-//         color: #dfdfdf;
-//     }
-//     /* ~~~ active day styles ~~~ */
-//     .react-calendar__tile--range {
-//         box-shadow: 0 0 6px 2px black;
-//     }
-//     /* ~~~ other view styles ~~~ */
-//     .react-calendar__year-view__months,
-//     .react-calendar__decade-view__years,
-//     .react-calendar__century-view__decades {
-//         display: grid !important;
-//         grid-template-columns: 20% 20% 20% 20% 20%;
-
-//         &.react-calendar__year-view__months {
-//             grid-template-columns: 33.3% 33.3% 33.3%;
-//         }
-
-//         .react-calendar__tile {
-//             max-width: initial !important;
-//         }
-//     }
-// `
-
-// const renderDay = (date, _view) => {
-//     const activitiesOnDay = activities?.filter((activity) => {
-//         return dayjs(activity.date).isSame(date, 'day')
-//     })
-
-//     return (
-//         <Box position="relative" height="100%">
-//             {activitiesOnDay.length > 0 && <StyledDot position="absolute" top={0} left={0} />}
-//             <Typography>{dayjs(date).format('dd D')}</Typography>
-//         </Box>
-//     )
-// }
