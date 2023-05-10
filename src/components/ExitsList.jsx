f45814b9554ad4f95e762f7e6fb9fec6799197d8
nicolasminahk <nico.minahk@gmail.com>
2023-05-10 14:30:50 -0300

    Depure component

--- src/components/ExitsList.jsx
+++ src/components/ExitsList.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { gql, useQuery, useMutation } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import ExitView from './ExitView'
-import { Box, Grid, useTheme } from '@mui/material'
-import backgroundImage from '../assets/paisaje3.png'
+import { Box, useTheme } from '@mui/material'
 
 const ALL_SALIDAS = gql`
     query AllSalidas {
@@ -19,7 +18,6 @@ const ALL_SALIDAS = gql`
 
 const ExitsList = () => {
     const { loading, error, data } = useQuery(ALL_SALIDAS)
-    console.log('daata', data?.allSalidas)
     const theme = useTheme()
 
     return (
@@ -50,10 +48,7 @@ const ExitsList = () => {
                             id={view.id}
                             description={view.description}
                             price={view.price}
-                            image={
-                                // 'https://img.freepik.com/foto-gratis/fondo-paisaje-montanas-ilustracion-puesta-sol_53876-105317.jpg?w=1800&t=st=1678219849~exp=1678220449~hmac=f5c53643de94674bf7f0bdbcb527a355d72a5a9d74dbd65f61f9788332e7db73'
-                                decodeImage(view.image)
-                            }
+                            image={decodeImage(view.image)}
                         />
                     )
                 })}
