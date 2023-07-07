4ad451af0b4c9cef56365ccfa095c5b786bdffc9
nicolasminahk <nico.minahk@gmail.com>
2023-07-07 16:15:59 -0300

    New Background

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
         <>
             <Box
                 sx={{
-                    backgroundImage: `url(${process.env.PUBLIC_URL}/fondo3.png)`,
+                    backgroundImage: `url(${process.env.PUBLIC_URL}/fondo7.jpg)`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
